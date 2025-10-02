943c51c7027d850a1d2da2e2ff4cc576627d91ff
Johan Taborda <johan.david.taborda@gmail.com>
2025-10-01 22:20:05 -0500

    Se implementa endpoint para el cierre de sesión del usuario.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 
 router.post('/login', authController.loginUser);
-router.post('/logout', (req, res) => {res.json({ message: 'Sesión cerrada exitosamente' });});
+router.post('/logout', authController.logoutUser);
 
 module.exports = router;
